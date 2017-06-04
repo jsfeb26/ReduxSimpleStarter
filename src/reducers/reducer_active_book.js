ef8c2759b026d0c7436ada37c6a5687f9e7389a6
Jason Stinson <jsfeb26@gmail.com>
2017-06-04 11:25:14 -0400

    book list redux example

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// state is not application state it's only state for this reducer
+export default function(state = null, action) {
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+
+  return state;
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'Javascript The Good Parts', pages: 85 },
+    { title: 'Reminiscences of a Stock Operator', pages: 300 },
+    { title: 'Market Wizards', pages: 400 },
+    { title: 'Deep Work', pages: 100 },
+    { title: 'New Market Wizards', pages: 450 }
+  ]
+}
